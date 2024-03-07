2c2e4e9971aaafb72519f580804faa27fe2eb167
Xapadoan <HollyolGamu@hotmail.com>
2024-03-07 18:49:05 +0100

    reset confirmation email

--- service/src/controllers/integrations/initReset.ts
+++ service/src/controllers/integrations/initReset.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
 import { Failable, HTTPError, ResetInitServiceInput } from 'authservice-shared';
 import { v4 as uuid } from 'uuid';
+import { createTransport } from 'nodemailer';
 import knex from '@data';
 import { User } from '@lib/types';
 import { redisClient } from '@lib/redisClient';
+
+const { HOST, SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS } = process.env;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function validate(body: any): body is ResetInitServiceInput {
@@ -11,20 +14,51 @@ function validate(body: any): body is ResetInitServiceInput {
   return true;
 }
 
+async function sendConfirmationEmail({
+  email,
+  SVCResetInitToken,
+}: {
+  email: string;
+  SVCResetInitToken: string;
+}) {
+  const transport = createTransport({
+    host: String(SMTP_HOST),
+    port: Number(SMTP_PORT),
+    secure: Number(SMTP_PORT) === 465,
+    auth: {
+      user: SMTP_USER,
+      pass: SMTP_PASS,
+    },
+  });
+
+  await transport.sendMail({
+    from: '"Auth Service" <reset@authservice.dev>',
+    to: email,
+    subject: 'Reset your identity card',
+    text: 'Hello World !',
+    html: `
+      <h1>Reset your cardId</h1>
+      <p>Click 
+      <a href=${HOST}/reset/confirm?SVCResetInitToken=${SVCResetInitToken}>here</a>
+       to confirm and start the reset process. If the link doesn't work copy paste
+       the following in a new tab:<br/>
+      ${HOST}/reset/confirm?SVCResetInitToken=${SVCResetInitToken}
+      </p>
+    `,
+  });
+}
+
 export async function initReset(
   req: Request,
   res: Response<HTTPError | Failable>
 ) {
   try {
-    console.log('RESET');
     if (!req.integration) {
       return res.status(401).json({ error: 'Missing Authentication' });
     }
-    console.log('integration OK');
     if (!validate(req.body)) {
       return res.status(400).json({ error: 'Bad Body' });
     }
-    console.log('Body OK');
     const { email } = req.body;
     const user: User = await knex('users')
       .select('*')
@@ -35,8 +69,10 @@ export async function initReset(
     }
     const SVCResetInitToken = uuid();
     await redisClient.set(`reset:${SVCResetInitToken}`, String(user.id), 600);
-    // await redisClient.set(`reset:${user.id}`, EACResetToken);
-    // Send mail now
+    await sendConfirmationEmail({
+      email,
+      SVCResetInitToken,
+    });
     return res.json({ success: true });
   } catch (error) {
     console.error('Reset card init failed: ', error);
