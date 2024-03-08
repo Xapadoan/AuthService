a6c83bb0102feb54a29feded63a54a4b40e01f58
Xapadoan <HollyolGamu@hotmail.com>
2024-03-08 16:06:42 +0100

    Add tests workflow

--- service/src/data/knexfile.ts
+++ service/src/data/knexfile.ts
@@ -20,15 +20,7 @@ export default {
   development: { ...commonConfig, debug: true },
   local: { ...commonConfig, debug: true },
   test: {
-    client: 'mysql2',
-    connection: {
-      host: process.env.DB_TEST_HOST,
-      port: Number(process.env.DB_TEST_PORT),
-      database: process.env.DB_TEST_NAME,
-      user: process.env.DB_TEST_USER,
-      password: process.env.DB_TEST_PASS,
-      charset: 'utf8mb4',
-    },
+    ...commonConfig,
     migrations: {
       directory: 'src/data/migrations',
     },
